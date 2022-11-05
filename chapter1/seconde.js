fdaf6f072f828c5e2896d3382a0c2c24f8382286
himchan94 <himchan9494@gmail.com>
2022-11-05 20:38:49 +0900

    feat: Apply second refact

---
+++ chapter1/seconde.js
@@ -0,0 +1,97 @@
+// amountFor이란 함수를 분리했다.
+// result 변수 적용
+// aPerformance 파라미터명 변경
+// play 변수 제거
+
+const plays = {
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
+};
+
+const invoices = {
+  customer: "BigCo",
+  performances: [
+    {
+      playID: "hamlet",
+      audience: 55,
+    },
+    {
+      playID: "as-like",
+      audience: 35,
+    },
+    {
+      playID: "othello",
+      audience: 40,
+    },
+  ],
+};
+
+function statment(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCredisFor(perf);
+
+    //  청구내력을 출력한다.
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += amountFor(perf);
+  }
+  result += `총액 ${usd(totalAmount)}\n`;
+  result == `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+function amountFor(aPerformance) {
+  let result = 0;
+
+  switch (playFor(aPerformance).type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+
+    default:
+      throw new Error(`알 수 없는 장르 :${playFor(aPerformance).type}`);
+  }
+
+  return result;
+}
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
+}
+
+function volumeCredisFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
+const value = statment(invoices, plays);
+
+console.log(value);
